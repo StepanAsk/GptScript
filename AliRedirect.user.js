708bd334877ef4e78e52b0be3206fe59d68e8276
StepanAsk <65081145+StepanAsk@users.noreply.github.com>
2024-12-14 14:37:08 +0530

    Create AliRedirect.user.js

---
+++ AliRedirect.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         AliExpress Domain Changer
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Автоматически заменяет домен aliexpress.com на aliexpress.ru
+// @author       SaintAsk
+// @match        *://*.aliexpress.com/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Проверяем, находится ли пользователь на домене aliexpress.com
+    if (window.location.hostname.includes('aliexpress.com')) {
+        // Заменяем домен на aliexpress.ru
+        let newUrl = window.location.href.replace('aliexpress.com', 'aliexpress.ru');
+        window.location.replace(newUrl);
+    }
+})();
